5243ab3bc736eb8599b90a2364c4de1fa8c658e4
Pan <dengpan.yuan.cool@gmailcom>
2017-11-13 23:13:43 +0800

    add code parser

--- complie.py
+++ complie.py
@@ -1,7 +1,21 @@
 import mistune
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters import html
 
 
+class HighlightRenderer(mistune.Renderer):
+    def block_code(self, code, lang=None):
+        if not lang:
+            return '\n<pre><code>%s</code></pre>\n' % \
+                   mistune.escape(code)
+        lexer = get_lexer_by_name(lang, stripall=True)
+        formatter = html.HtmlFormatter()
+        return highlight(code, lexer, formatter)
+
+
+renderer = HighlightRenderer()
+markdown = mistune.Markdown(renderer=renderer)
 file = open("/Users/yuanxiansen/Desktop/school_affair/Y2:1/CSE101/ASS/ASS3/README.md", "rb")
 string_result = file.read().decode("utf-8")
-test = mistune.markdown(string_result)
-print(test)
+print(markdown(string_result))
